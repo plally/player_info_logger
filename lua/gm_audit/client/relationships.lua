2ce8db4165463267f0b98544c767bf0b3b343441
Pierce <pierce@vulpes.dev>
2022-12-02 15:03:17 -0500

    remove hook in clientside

--- lua/gm_audit/client/relationships.lua
+++ lua/gm_audit/client/relationships.lua
@@ -1,15 +1,10 @@
+timer.Create("GMAudit_Upload", 120, 0, function()
+    local relationships = {}
+    for _, v in pairs(player.GetHumans()) do
+        relationships[v:SteamID64()] = v:GetFriendStatus()
+    end
 
-require( "playerload" )
-
-hook.Add( "PlayerFullLoad", "OnPlayerFullyLoaded", function( ply )
-	timer.Create("GMAudit_Upload", 120, 0, function()
-        local relationships = {}
-        for _, v in pairs(player.GetHumans()) do
-            relationships[v:SteamID64()] = v:GetFriendStatus()
-        end
-
-        net.Start("GMAudit_Relationships")
-            net.WriteTable(relationships)
-        net.SendToServer()
-    end)
-end )
+    net.Start("GMAudit_Relationships")
+        net.WriteTable(relationships)
+    net.SendToServer()
+end)
